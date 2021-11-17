f7209f356ce7cd7c9eb72d7a98d72f3acad22cfc
unaxlazka <ulazkanotegi001@ikasle.ehu.eus>
2021-11-17 22:42:53 +0100

    Proba berriak, botoia disable adierazteko erabilpenak inplementatzen

--- BetsJSF_Unax/src/gui/bean/CreateQuestionBean.java
+++ BetsJSF_Unax/src/gui/bean/CreateQuestionBean.java
@@ -10,28 +10,54 @@ import javax.faces.event.AjaxBehaviorEvent;
 import bl.bean.FacadeBean;
 import businessLogic.BLFacade;
 import domain.Event;
+import exceptions.EventFinished;
+import exceptions.QuestionAlreadyExist;
 
 public class CreateQuestionBean {
 
 	private Date data;
-	
+
 	private String dataString;
 	private Vector<Event> gertaerak;
 	private Event gertaera;
-	
+	private String galBer;
+	private double aposMin;
+	private boolean disable;
+
+	public double getAposMin() {
+		return aposMin;
+	}
+
+	public void setAposMin(double aposMin) {
+		this.aposMin = aposMin;
+	}
+
+	public String getGalBer() {
+		return galBer;
+	}
+
+	public void setGalBer(String galBer) {
+		this.galBer = galBer;
+	}
+
 	private String hutsa;
-	
+
 	private BLFacade blFacade;
 
 	public CreateQuestionBean() {
 		this.dataString = "Gertaerak";
 		this.gertaerak = new Vector<Event>();
-		
+
 		this.hutsa = "Hutsa";
-		
+
+		this.disable = true;
+
+		this.galBer = "";
+		this.aposMin = 0;
+
 		this.blFacade = FacadeBean.getBusinessLogic();
 	}
-	
+
 	public String getHutsa() {
 		return hutsa;
 	}
@@ -71,7 +97,7 @@ public class CreateQuestionBean {
 	public void setGertaera(Event gertaera) {
 		this.gertaera = gertaera;
 	}
-	
+
 	public void getEvents() {
 		Integer eg = data.getDate();
 		String hil = getHilabeteIzena();
@@ -82,16 +108,42 @@ public class CreateQuestionBean {
 			this.dataString = "Gertaerak: " + urt + " " + hil + " " + eg;
 			this.gertaerak = gerList;
 		} else {// Taularen gaineko testuaren balioa aldatu behar diot
+			this.gertaera=null;//Hau egiten dut data berri bat aukeratzerakoan aurreko gertaera jada gorde ez dadin.
 			this.dataString = "Ez daude gertaerak: " + urt + " " + hil + " " + eg;
 			this.gertaerak = new Vector<Event>();// Aurretik zeuden gertaerak ezabatu
 		}
 	}
-	
+
 	public void eguneratzeaIkusi(AjaxBehaviorEvent event) {
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage("Gertaera eguneratu da:" + gertaera.getDescription() + "/" + gertaera.getEventNumber()));
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+				"Gertaera eguneratu da:" + gertaera.getDescription() + "/" + gertaera.getEventNumber()));
+	}
+
+	public void disableAztertu(AjaxBehaviorEvent event) {
+		if (this.aposMin != 0 && this.galBer != "" && this.gertaera != null) {
+			this.disable = false;
+		} else {
+			this.disable = true;
+		}
 	}
 	
+	public void eguneratzeaIkusiEtaDisableAztertu(AjaxBehaviorEvent event) {
+		eguneratzeaIkusi(event);
+		disableAztertu(event);
+	}
+
+	public void galderaSortu() throws EventFinished, QuestionAlreadyExist {
+		this.blFacade.createQuestion(gertaera, galBer, (float) aposMin);
+	}
+
+	public boolean isDisable() {
+		return disable;
+	}
+
+	public void setDisable(boolean disable) {
+		this.disable = disable;
+	}
+
 	public String getHilabeteIzena() {
 		int hil = data.getMonth();
 		if (hil == 0)
